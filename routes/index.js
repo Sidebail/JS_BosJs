6f0f794bbfef72ef524491c3ad64f5d6130038dd
Nathaniel Fisher <nathaniel.fisher@mygeorgian.ca>
2019-07-23 20:58:29 -0400

    add saving enemies and player to DB

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+const Enemy = require('../models/enemy');
+const Player = require('../models/player');
 var express = require('express');
 var router = express.Router();
 var nickname;
@@ -18,9 +20,16 @@ router.get('/gameview', function(req,res,next){
   res.render('gameview', { title: 'Defeat Enemies!', playerName: nickname });
 });
 
+// this receives data from game about enemies and player
 router.post('/gameview', function(req, res){
-  finalscore = req.body.score; 
-  //console.log(finalscore);
+  //finalscore = req.body.score; 
+  // create 3 enemy models with data from gameview
+  let enemy1 = new Enemy({ name: req.body.e1name, health: req.body.e1maxHealth, attack: req.body.e1attack, armor: req.body.e1armor,});
+  let enemy2 = new Enemy({ name: req.body.e2name, health: req.body.e2maxHealth, attack: req.body.e2attack, armor: req.body.e2armor,});
+  let enemy3 = new Enemy({ name: req.body.e3name, health: req.body.e3maxHealth, attack: req.body.e3attack, armor: req.body.e3armor,});
+
+  let player = new Player({ name: req.body.name, health: req.body.maxHealth, attack: req.body.attack , armor: req.body.armor, level: req.body.level, lastThreeEnemies: [enemy1, enemy2, enemy3]});
+  player.save();
 });
 
 router.get('/gameover', function(req,res,next){
