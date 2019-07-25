4c0e9bb5db744662f7f527fb59d900e00bb8e378
Sidebail <tfg.devteam@gmail.com>
2019-07-25 12:21:33 -0400

    Added mongoose-findorcreate. Fixed user model.

--- models/user.js
+++ models/user.js
@@ -1,15 +1,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const findorcreate = require('mongoose-findorcreate');
 const passportLocalMongoose = require('passport-local-mongoose');
 //const Enemy = require('./enemy');
 
 const userSchema = new Schema({
   username: String,
   password: String,
-  highestScore: Number
+  highestScore: Number,
+  googleId: String,
+  githubId: String
 });
 
+userSchema.plugin(findorcreate);
 userSchema.plugin(passportLocalMongoose);
+
 
 const User = mongoose.model('User', userSchema);
 
